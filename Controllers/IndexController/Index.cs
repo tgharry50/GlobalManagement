18e457fab44364a8323792fa9445b00ff9520083
mikolaj <mikolaj@mikolja.168.1.117>
2024-10-26 09:36:57 +0200

    0.0.2

---
+++ Controllers/IndexController/Index.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalManagement.Controllers
+{
+    [ApiController]
+    [Route("index")]
+    public class Index : ControllerBase
+    {
+        private readonly GlobalManagement _globalManagement;
+        public Index(GlobalManagement globalManagement){
+            globalManagement = _globalManagement;
+        }
+
+        public async Task<IActionResult> Get(){
+            try{
+                
+            }
+        }
+    }
+
+}
